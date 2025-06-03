a5adc55766a6b4cc8b611e500a8476f5bf182a16
ManojNambiraj <postboxmano@gmail.com>
2025-06-03 20:53:02 +0530

    Python class2 codes

---
+++ class2/demo.py
@@ -0,0 +1,29 @@
+# Output statements:
+
+"""
+    print("Hello", end=',,,,,')
+    print("Good evening...!", end=" ")
+"""
+
+    # print("EverGreen", end='\n')        # End Stmts
+
+    # print("hello", "world", "Good", "Evening", sep='/') # Sep stmts
+
+
+# Multi line string:
+
+    # print("""prints the values to a stream, or to sys.stdout by default.
+    # string inserted between values, default a space.
+    # string appended after the last value, default a newline.
+    # a file-like object (stream); defaults to the current""")
+
+# Escape Sequence:
+
+    # print('''string "appended" after the\\n last value, default a newline.''')
+
+# Variables:
+
+my_age_in_myshcool_days = 500
+
+print(my_age_in_myshcool_days)
+
