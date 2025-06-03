e62f5c3a32c480a8eed870efc7c1c8fb56067d3f
ManojNambiraj <postboxmano@gmail.com>
2025-06-03 21:22:41 +0530

    datatypes and Operators

---
+++ class3/datatypes.py
@@ -0,0 +1,38 @@
+# Datatypes:
+
+    # # a = 10           # integer (numbers)
+    # # a = 10.5         # float
+    # # a = 'Ram'        # string (Text)
+    # # a = False        # boolean
+
+    # print(a)
+    # print(type(a))
+
+# Operators:
+
+"""
+    1. Arithmetic OP
+        (+, -, *, /, %, **, //)
+
+    2. Assignment OP
+    3. Comparison OP
+    4. Logical OP
+    5. Identity OP
+    6. Membership OP
+    7. Bitwise OP
+"""
+
+# 1. Arithmetic OP:  (+, -, *, /, %, **, //)
+
+    # a = 10
+    # b = 7
+
+    # result = a + b
+    # result = a - b
+    # result = a * b
+    # result = a / b
+    # result = a % b
+    # result = a ** b
+    # result = a // b
+
+    # print(result)
